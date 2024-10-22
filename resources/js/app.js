a437aedcedbf9e0a4bd2d72faa404a4071d20450
jead work laptop <jfuentes012345@gmail.com>
2024-10-22 08:56:23 +0800

    fix the tooltip

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,5 @@
 import './bootstrap';
-import './../assets/js/color-modes.js'
+import './../assets/js/color-modes.js';
+import 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js';
+
 
